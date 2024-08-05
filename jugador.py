3e576f28e7302646d2d0532c41bd996b34dd3ace
lauraax <64974256+lauraax@users.noreply.github.com>
2024-08-05 07:25:37 -0500

    Add files via upload

---
+++ jugador.py
@@ -0,0 +1,8 @@
+from programa import *
+
+class Jugador(Equipo):
+    def __init__(self):
+
+class Suplente(Equipo):
+    def __init__(self):
+        
---
+++ programa.py
@@ -0,0 +1,15 @@
+class Equipo():
+    def __init__(self, coach):
+        self.coach = coach
+        self.jugadores = []
+        self.suplentes = []
+    
+    def run(self):
+        print("Bienvenido al programa para armar tu equipo.")
+    
+
+if __name__ == '__main__': 
+
+        
+
+
